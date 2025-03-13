7ce82d8b59942ba10a3aa754ebbab0ba7ce52f86
malory171 <malory171@gmail.com>
2025-03-13 22:01:39 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Dictionary</h1>
+        <p>Type a word to know its meaning </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
